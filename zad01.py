be3cce3c0aeb51c4f31bd2c6ba0e11e87eb58dd6
Richard Körösi <korosi.richard@gmail.com>
2024-03-22 14:09:31 +0100

    jesus

--- zad01.py
+++ zad01.py
@@ -1,3 +1,20 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+
+sys.setrecursionlimit(999999999)
+
+
+class Node:
+    def __init__(self, key, left=None, right=None):
+        self.key = key
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f'Node(Key:{self.key},Left:{self.left},Right{self.right})'
+
+
 def create_triplets_k_p_q(file_path='dictionary.txt'):
     triplets = []
     qs = []
@@ -11,22 +28,25 @@ def create_triplets_k_p_q(file_path='dictionary.txt'):
     keys_lines = [line for line in lines if line[0] > 50000]
 
     full_freq = sum([line[0] for line in lines])
-    # lines = [[line[0] / full_freq, line[1]] for line in lines]
-    # keys_lines = [[line[0] / full_freq, line[1]] for line in keys_lines]
+    lines = [[line[0] / full_freq, line[1]] for line in lines]
+    keys_lines = [[line[0] / full_freq, line[1]] for line in keys_lines]
 
     qs.append(find_q0(keys_lines, lines))
 
     for key_line in keys_lines:
         qs.append(find_qi(keys_lines, lines, key_line))
 
-    print(f'My sum: {sum([key_line[0] for key_line in keys_lines]) + sum(qs)}')
-    print(f'Frequency sum: {full_freq}')
+    # print(f'My sum: {sum([key_line[0] for key_line in keys_lines]) + sum(qs)}')
+    # print(f'Frequency sum: {full_freq}')
+    #
+    # print(f'Sum Qs:{sum(qs)}')
+    # print(f'My Ps:{sum([key_line[0] for key_line in keys_lines])}')
 
     triplets.append(['', 0, qs[0]])
     for i in range(len(keys_lines)):
         triplets.append([keys_lines[i][1], keys_lines[i][0], qs[i + 1]])
 
-    print(f'Sum in triplets: {sum([triplet[1] + triplet[2] for triplet in triplets])}')
+    # print(f'Sum in triplets: {round(sum([triplet[1] + triplet[2] for triplet in triplets]), 12)}')
 
     return triplets
 
@@ -74,10 +94,139 @@ def abduls_triplets():
     return triplets
 
 
+def calculate_tables(n, p, q):
+    c = [[0 for _ in range(n)] for _ in range(n)]
+    w = [[0 for _ in range(n)] for _ in range(n)]
+    root = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(0, n):
+        w[i][i] = q[i]
+
+    for l in range(1, n):
+        for i in range(0, n - l):
+            j = i + l
+            c[i][j] = float('inf')
+            w[i][j] = w[i][j - 1] + p[j] + q[j]
+            for r in range(i, j):
+                t = c[i][r] + c[r + 1][j] + w[i][j]
+                if t < c[i][j]:
+                    c[i][j] = t
+                    root[i][j] = r
+
+    return c, root
+
+
+def calculate_tables2(n, p, q):
+    e = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
+    w = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
+    root = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 2):
+        e[i][i - 1] = q[i - 1]
+        w[i][i - 1] = q[i - 1]
+
+    for l in range(1, n + 1):
+        for i in range(1, n - l + 2):
+            j = i + l - 1
+            e[i][j] = float('inf')
+            w[i][j] = w[i][j - 1] + p[j] + q[j]
+            for r in range(i, j + 1):
+                t = e[i][r - 1] + e[r + 1][j] + w[i][j]
+                if t < e[i][j]:
+                    e[i][j] = t
+                    root[i][j] = r
+
+    return e, root
+
+
+def build_tree(dp_root, lines, start, end):
+    if start == end:
+        return None
+    root_index = dp_root[start][end]
+    key = lines[root_index][0]
+    left = build_tree(dp_root, lines, start, root_index)
+    right = build_tree(dp_root, lines, root_index + 1, end)
+    return Node(key, left, right)
+
+
+def build_tree2(dp_root, lines, start, end):
+    if start > end:
+        return None
+    root_index = dp_root[start][end] - 1  # Adjust for 0-based indexing
+    key = lines[root_index][0]
+    left = build_tree2(dp_root, lines, start, root_index - 2)  # Adjust indices accordingly
+    right = build_tree2(dp_root, lines, root_index + 2, end)  # Adjust indices accordingly
+    return Node(key, left, right)
+
+
+def add_nodes_edges(G, node, pos=None, x=0, y=0, layer=1):
+    if pos is None:
+        pos = {}
+    pos[node.key] = (x, y)
+    if node.left is not None:
+        G.add_edge(node.key, node.left.key)
+        pos = add_nodes_edges(G, node.left, pos, x - 1 / layer, y - 1, layer * 2)
+    if node.right is not None:
+        G.add_edge(node.key, node.right.key)
+        pos = add_nodes_edges(G, node.right, pos, x + 1 / layer, y - 1, layer * 2)
+    return pos
+
+
+def print_tree(tree):
+    G = nx.DiGraph()
+    pos = add_nodes_edges(G, tree)
+    nx.draw(G, pos, with_labels=True, node_color='white', font_size=6, arrows=False)
+    plt.show()
+
+
+def compare_trees(tree, tree2):
+    if tree is None and tree2 is None:
+        return True
+    if tree is None or tree2 is None:
+        return False
+    return compare_trees(tree.left, tree2.left) and compare_trees(tree.right, tree2.right)
+
+
+def binary_search(tree, word, comparisons=0):
+    if tree is None:
+        return False, comparisons
+    comparisons += 1
+    if tree.key == word:
+        return True, comparisons
+    if tree.key > word:
+        return binary_search(tree.left, word, comparisons)
+    return binary_search(tree.right, word, comparisons)
+
+
 def main():
-    # triplets = create_triplets_k_p_q()
-    abdul = abduls_triplets()
-    print(abdul)
+    triplets = create_triplets_k_p_q()
+    triplets_to_print = triplets.copy()
+    triplets_to_print.pop(0)
+    c1, root1 = calculate_tables(len(triplets) - 1, [triplet[1] for triplet in triplets],
+                                 [triplet[2] for triplet in triplets])
+
+    print(c1[0][-1])
+
+    c2, root2 = calculate_tables2(len(triplets) - 1, [triplet[1] for triplet in triplets],
+                                  [triplet[2] for triplet in triplets])
+
+    print(c2[1][-2]) \
+        # remove first element in root2
+    root2 = root2[1:]
+    for i in range(len(root2)):
+        for j in range(len(root2[i])):
+            root2[i][j] -= 1
+
+    tree = build_tree(root1, triplets_to_print, 0, len(triplets_to_print) - 1)
+    print_tree(tree)
+    tree2 = build_tree(root2, triplets_to_print, 0, len(triplets_to_print) - 1)
+    print_tree(tree2)
+
+    print(compare_trees(tree, tree2))
+    for word in triplets:
+        print(word[0], binary_search(tree, word[0]))
+
+    return True
 
 
 main()
